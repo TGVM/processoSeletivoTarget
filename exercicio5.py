cc013ca2a0e9b1e324442a4aa50fcae77e7a8b55
TGVM <tgvm96@gmail.com>
2025-02-28 11:35:27 -0300

    exercício 5

--- exercicio5.py
+++ exercicio5.py
@@ -1,5 +1,16 @@
 # 5) Escreva um programa que inverta os caracteres de um string.
 
 # IMPORTANTE:
-# a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+# a) Essa string pode ser informada através de qualquer entrada de 
+# sua preferência ou pode ser previamente definida no código;
 # b) Evite usar funções prontas, como, por exemplo, reverse;
+
+
+palavra = str(input("Digite uma palavra para ser invertida: "))
+
+palavraInversa = ""
+
+for i in range(len(palavra)-1, -1, -1):
+    palavraInversa = palavraInversa + palavra[i]
+
+print(palavraInversa)
