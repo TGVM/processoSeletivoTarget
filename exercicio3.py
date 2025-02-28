e9f331ef02de509110904cd4ee630f183216cae8
TGVM <tgvm96@gmail.com>
2025-02-28 11:21:26 -0300

    exercício 3

--- exercicio3.py
+++ exercicio3.py
@@ -13,15 +13,55 @@
 # feriados. Estes dias devem ser ignorados no cálculo da média;
 
 import json
+import math
 
-arquivo = open ('dados.json')
-jsonVetor = json.load(arquivo)
+
+
 listaFaturamento = []
+menorFaturamento = math.inf
+diaMenorFat = ""
+maiorFaturamento = -math.inf
+diaMaiorFat = ""
+mediaMensal = 0
+contagemDias = 0
 
-for item in jsonVetor:
-    dia = {"dia":None, "valor":None}
-    dia['dia'] = item['dia']
-    dia['valor'] = item['valor']
-    listaFaturamento.append(dia)
 
-print(listaFaturamento)
+def LeituraDoJSON():
+
+    arquivo = open ('dados.json')
+    jsonVetor = json.load(arquivo)
+    for item in jsonVetor:
+        dia = {"dia":None, "valor":None}
+        dia['dia'] = item['dia']
+        dia['valor'] = item['valor']
+        listaFaturamento.append(dia)
+
+def RealizarOperacoes():
+    global maiorFaturamento
+    global menorFaturamento
+    global diaMaiorFat
+    global diaMenorFat
+    global mediaMensal
+    global contagemDias
+    
+    for item in listaFaturamento:
+        if(item['valor']>0.0):
+            if(item['valor'] > maiorFaturamento):
+                maiorFaturamento = item['valor']
+                diaMaiorFat = item['dia']
+            if(item['valor'] < menorFaturamento):
+                menorFaturamento = item['valor']
+                diaMenorFat = item['dia']
+            mediaMensal += item['valor']
+    mediaMensal = mediaMensal/len(listaFaturamento)
+
+    for item in listaFaturamento:
+        if(item['valor'] > mediaMensal):
+            contagemDias+=1
+
+
+LeituraDoJSON()
+RealizarOperacoes()
+print("O menor valor de faturamento foi no dia "+str(diaMenorFat)+", com "+str(menorFaturamento)+"R$, sem contar finais de semana e feriados.")
+print("O maior valor de faturamento foi no dia "+str(diaMaiorFat)+", com "+str(maiorFaturamento)+"R$")
+print("Número de dias em que a média de faturamento mensal ("+str(mediaMensal)+") foi superada: "+str(contagemDias))
